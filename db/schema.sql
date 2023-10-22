7df0957dbf3cd122c814df41729c7673dca1a1dc
Mahmoud Nabil <mahmoudmuhamednabil@gmail.com>
2023-10-22 08:28:54 -0400

    adding schema and seeds code to files

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE if EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
+
+CREATE Table department (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) UNIQUE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN key (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE,
+    manager_id INT,
+    FOREIGN key (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (name)
+VALUES ("Security"), ("Reception"), ("Engineering"), ("Human Resources"), ("Accounting");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Head of Security", 25000.00, 1), ("Accountant", 400000.00, 5), ("Engineer", 600000.00, 3), ("HR Manger", 700000.00, 4), ("Receptionest", 400000.00, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Erling", "Halland", 1, 3), ("Marcos", "Alonso", 1, 1), ("Kevin", "Debruyne", 3, 2), ("Bukayo", "Saka", 5, 2), ("Declan", "Rice", 5, 2);
